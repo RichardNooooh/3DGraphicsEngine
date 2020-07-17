83eb2bbede1681c3c9ae9662c9d4253ecaedcd56
Ice <richard.jun.noh@gmail.com>
2020-07-16 23:05:14 -0500

    Add blank template window
    There is no functionality currently since it's in a permanent while()
    loop.

--- SimpleGraphicsEngine/WinMain.cpp
+++ SimpleGraphicsEngine/WinMain.cpp
@@ -1,10 +1,39 @@
 #include <Windows.h>
 
-
+//Modified from ChiliTomatoNoodle's C++ DirectX Tutorial: https://www.youtube.com/watch?v=nQTiSLiNyk4
 int CALLBACK WinMain(HINSTANCE hInstance,
 					 HINSTANCE hPrevInstance,
 					 LPSTR lpCmdLine,
 					 int nCmdShow)
 {
+	const LPCWSTR pClassName = L"Test";
+
+	//register window class
+	WNDCLASSEX wc = {0};
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	//create and show window instance
+	DWORD windowStyles = WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU;
+	HWND hWnd = CreateWindowEx(0, pClassName,
+							   L"Test Window", windowStyles,
+							   200, 200, 640, 480,
+							   nullptr, nullptr, hInstance, nullptr);
+
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
+
 	return 0;
 }
