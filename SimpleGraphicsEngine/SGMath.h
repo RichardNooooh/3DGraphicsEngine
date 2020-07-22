a74a0be33d085958727ef1abe3c53e0c95b68a3c
Ice <richard.jun.noh@gmail.com>
2020-07-22 16:12:37 -0500

    Rename Vector3D to Vector3
    The extra D at the end was unnecessary to type. Vector3 is a lot more
    comfortable to use.

--- SimpleGraphicsEngine/SGMath.h
+++ SimpleGraphicsEngine/SGMath.h
@@ -1,18 +1,19 @@
 #pragma once
 
-struct Vector3D 
+struct Vector3 
 {
 	float x, y, z;
-	Vector3D(float x, float y, float z) : x(x), y(y), z(z) {};
+	Vector3(float x, float y, float z) : x(x), y(y), z(z) {};
 	
-	Vector3D operator+(Vector3D other) 
+	Vector3 operator+(Vector3 other) 
 	{
-		return Vector3D(x + other.x, y + other.y, z + other.z);
+		return Vector3(x + other.x, y + other.y, z + other.z);
 	}
 
-	Vector3D operator-(Vector3D other)
+	Vector3 operator-(Vector3 other)
 	{
-		return Vector3D(x - other.x, y - other.y, z - other.z);
+		return Vector3(x - other.x, y - other.y, z - other.z);
 	}
 
+	
 };
