7d2b067d80127e31881df49722a2efa649281216
Ice <richard.jun.noh@gmail.com>
2020-07-22 16:17:09 -0500

    Add DotProduct() to Vector3 and add const keywords

--- SimpleGraphicsEngine/SGMath.h
+++ SimpleGraphicsEngine/SGMath.h
@@ -5,15 +5,19 @@ struct Vector3
 	float x, y, z;
 	Vector3(float x, float y, float z) : x(x), y(y), z(z) {};
 	
-	Vector3 operator+(Vector3 other) 
+	Vector3 operator+(Vector3 other) const
 	{
 		return Vector3(x + other.x, y + other.y, z + other.z);
 	}
 
-	Vector3 operator-(Vector3 other)
+	Vector3 operator-(Vector3 other) const
 	{
 		return Vector3(x - other.x, y - other.y, z - other.z);
 	}
 
+	float DotProduct(Vector3 other) const
+	{
+		return x * other.x + y * other.y + z * other.z;
+	}
 	
 };
