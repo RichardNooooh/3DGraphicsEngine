fe784bcd1bcdb06a2410ff6fdc321bb234b01f04
Ice <richard.jun.noh@gmail.com>
2020-07-23 16:11:34 -0500

    Add Matrix44 struct
    This is a structure that represents a 4 by 4 matrix. Instead of a
    standard 4 by 4 2D array structure, a struct allows for operator
    overloading.

--- SimpleGraphicsEngine/SGMath.h
+++ SimpleGraphicsEngine/SGMath.h
@@ -30,10 +30,28 @@ struct Vector3
 
 struct Matrix44 
 {
-	float matrix[4][4];
+	float m[4][4] = { 0 };
+	//Matrix44(float input[4][4]);
+	//~Matrix44();
+
+	Matrix44 operator+ (Matrix44 other) const 
+	{
+		return { { {m[0][0] + other.m[0][0], m[0][1] + other.m[0][1], m[0][2] + other.m[0][3]},
+				   {m[1][0] + other.m[1][0], m[1][1] + other.m[1][1], m[1][2] + other.m[1][3]},
+				   {m[2][0] + other.m[2][0], m[2][1] + other.m[2][1], m[2][2] + other.m[2][3]},
+				   {m[3][0] + other.m[3][0], m[3][1] + other.m[3][1], m[3][2] + other.m[3][3]} } };
+	}
+
+	Matrix44 operator- (Matrix44 other) const
+	{
+		return { { {m[0][0] - other.m[0][0], m[0][1] - other.m[0][1], m[0][2] - other.m[0][3]},
+				   {m[1][0] - other.m[1][0], m[1][1] - other.m[1][1], m[1][2] - other.m[1][3]},
+				   {m[2][0] - other.m[2][0], m[2][1] - other.m[2][1], m[2][2] - other.m[2][3]},
+				   {m[3][0] - other.m[3][0], m[3][1] - other.m[3][1], m[3][2] - other.m[3][3]} } };
+	}
 };
 
 static const Matrix44 WorldToPerspectiveMatrix = { { {1, 0, 0, 0},
 													 {1, 0, 0, 0},
-												     {1, 0, 0, 0},
-												     {1, 0, 0, 0} } };
+													 {1, 0, 0, 0},
+													 {1, 0, 0, 0} } };
